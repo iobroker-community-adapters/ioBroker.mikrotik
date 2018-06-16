934052e4f1f813109c388c97f39ed682eccd5508
instalator <vvvalt@mail.ru>
2018-06-17 00:52:03 +0700

    fix error settings

--- admin/words.js
+++ admin/words.js
@@ -5,10 +5,10 @@ systemDictionary = {
 	"port":                   	{"en": "port", "de": "port", "ru": "Порт"},
 	"login":                   	{"en": "login", "de": "login", "ru": "Логин"},
 	"password":                	{"en": "password", "de": "password", "ru": "Пароль"},
-	"def":                		{"en": "default (8728)", "de": "default (8728)", "ru": "По-умолчанию (8728)"},
+	"def":                		{"en": "default (8728)", "de": "default (8728)", "ru": "по-умолчанию (8728)"},
 	"timeout":                  {"en": "timeout", "de": "Zeitüberschreitung", "ru": "Таймаут"},
-	"sec":                   	{"en": "sec", "de": "sec", "ru": "с"},
-	"ms":                   	{"en": "ms", "de": "ms", "ru": "мс"},
+	"sec":                   	{"en": "seconds", "de": "sekunden", "ru": "секунды"},
+	"ms":                   	{"en": "milliseconds", "de": "millisekunden", "ru": "миллисекунды"},
 	"poll":                   	{"en": "Data refresh interval", "de": "Daten Aktualisierungsintervall", "ru": "Интервал обновления данных"},
 	"on save adapter restarts with new config immediately": {
 		"de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
